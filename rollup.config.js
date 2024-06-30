0d39148a8e5922c555e12adc9bf4ed1e8535c18e
Kirill Greshnov <k.greshnov@innopolis.university>
2024-06-30 23:38:20 +0300

    Deploy test

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import typescript from "@rollup/plugin-typescript";
 export default {
     input: 'src/index.ts',
     output: {
-        file: 'dist/bundle.js',
+        file: 'src/dist/bundle.js',
         sourcemap: true,
         name: 'Bundle',
     },
