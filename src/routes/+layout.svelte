02a4ceaa7a7123db15a91de4a9274870c9806cd1
Kirill Greshnov <k.greshnov@innopolis.university>
2024-07-11 23:24:50 +0300

    Added tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,16 @@
 	import Footer from '$lib/components/footer/footer.svelte';
 </script>
 
+<meta 
+	name="author"
+	content="Kirill Greshnov"
+/>
+
+<meta 
+	name="description"
+	content="Computer Science student at Innopolis University"
+/>
+
 <Header />
 <slot />
 <div class="min-h-24" />
