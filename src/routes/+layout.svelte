cbfeaf5f29c1bb95437673e1f63c2fd1e83ebefc
Kirill Greshnov <k.greshnov@innopolis.university>
2024-07-11 17:34:56 +0300

    Initial create SvelteKit Project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import '../app.css'
+</script>
+
+<header>
+    <div class="bg-headerback">
+        Wassup
+    </div>
+</header>
+
+<slot />
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        headerback: '#8a2be2',
+        logomain: 'f0ffff',
+        logohover: "#ffc800",
+      }
+    }
+  },
+  plugins: []
+};
